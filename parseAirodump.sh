40a1a6a8de82b1ff42bdaef3da7036b3c76c4502
M4lal0 <malalocerrillo@gmail.com>
2020-11-05 11:43:30 -0600

    feat: updates
    CHANGES:
    - Agregar paleta de colores
    - Cambio de colores en los resultados
    - Descarga automatica del archivo oui.txt

--- parseAirodump.sh
+++ parseAirodump.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# By @m4lal0
+
 # Regular Colors
 Black='\033[0;30m'      # Black
 Red='\033[0;31m'        # Red
@@ -21,6 +23,28 @@ LBlue='\033[0;94m'      # Ligth Blue
 LPurple='\033[0;95m'    # Light Purple
 LCyan='\033[0;96m'      # Ligth Cyan
 
+# Bold
+BBlack='\033[1;30m'     # Black
+BGray='\033[1;37m'		# Gray
+BRed='\033[1;31m'       # Red
+BGreen='\033[1;32m'     # Green
+BYellow='\033[1;33m'    # Yellow
+BBlue='\033[1;34m'      # Blue
+BPurple='\033[1;35m'    # Purple
+BCyan='\033[1;36m'      # Cyan
+BWhite='\033[1;37m'     # White
+
+# Underline
+UBlack='\033[4;30m'     # Black
+UGray='\033[4;37m'		# Gray
+URed='\033[4;31m'       # Red
+UGreen='\033[4;32m'     # Green
+UYellow='\033[4;33m'    # Yellow
+UBlue='\033[4;34m'      # Blue
+UPurple='\033[4;35m'    # Purple
+UCyan='\033[4;36m'      # Cyan
+UWhite='\033[4;37m'     # White
+
 # Background
 On_Black='\033[40m'     # Black
 On_Red='\033[41m'       # Red
@@ -34,25 +58,25 @@ On_White='\033[47m'     # White
 if [[ "$1" && -f "$1" ]]; then
     FILE="$1"
 else
-    echo -e "\n${LBlue}[${Yellow}!${LBlue}] ${LGray}Especifica el fichero .csv a analizar${Color_Off}\n";
+    echo -e "\n${LBlue}[${BYellow}!${LBlue}] ${BYellow}Especifica el fichero .csv a analizar${Color_Off}\n";
     echo 'Uso:';
-    echo -e "\t${LGray}./parser.sh ${LCyan}Captura-01.csv${Color_Off}\n";
+    echo -e "\t${BGray}./parser.sh ${BCyan}Captura-01.csv${Color_Off}\n";
     exit
 fi
 
 test -f oui.txt 2>/dev/null
 
 if [ "$(echo $?)" == "0" ]; then
-    echo -e "\n${LGray}Número total de puntos de acceso: ${Red}`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){14}' $FILE | wc -l`${Color_Off}"
-    echo -e "${LGray}Número total de estaciones: ${Red}`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){5} ([A-Z0-9:]{17})|(not associated)' $FILE | wc -l`${Color_Off}"
-    echo -e "${LGray}Número total de estaciones no asociadas: ${Red}`grep -E '(not associated)' $FILE | wc -l`${Color_Off}"
-    echo -e "\n${LBlue}Puntos de acceso disponibles:${Color_Off}"
+    echo -e "\n${BGray}Número total de puntos de acceso: ${BRed}`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){14}' $FILE | wc -l`${Color_Off}"
+    echo -e "${BGray}Número total de estaciones: ${BRed}`grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){5} ([A-Z0-9:]{17})|(not associated)' $FILE | wc -l`${Color_Off}"
+    echo -e "${BGray}Número total de estaciones no asociadas: ${BRed}`grep -E '(not associated)' $FILE | wc -l`${Color_Off}"
+    echo -e "\n${BBlue}Puntos de acceso disponibles:${Color_Off}"
 
     while read -r line ; do
         if [ "`echo "$line" | cut -d ',' -f 14`" != " " ]; then
-            echo -e "${LGray}" `echo -e "$line" | cut -d ',' -f 14` "${Color_Off}"
+            echo -e "${BWhite}${On_Black}" `echo -e "$line" | cut -d ',' -f 14` "${Color_Off}"
         else
-            echo -e " ${Red}No es posible obtener el nombre de la red (ESSID)${Color_Off}"
+            echo -e " ${BRed}No es posible obtener el nombre de la red (ESSID)${Color_Off}"
         fi
 
         fullMAC=`echo "$line" | cut -d ',' -f 1`
@@ -65,13 +89,13 @@ if [ "$(echo $?)" == "0" ]; then
         if [ "$result" ]; then
             echo -e "\tVendor: `echo "$result" | cut -f 3`"
         else
-            echo -e "\tVendor: ${Red}Información no encontrada en la base de datos${Color_Off}"
+            echo -e "\tVendor: ${BRed}Información no encontrada en la base de datos${Color_Off}"
         fi
 
         is5ghz=`echo "$line" | cut -d ',' -f 4 | grep -i -E '36|40|44|48|52|56|60|64|100|104|108|112|116|120|124|128|132|136|140'`
 
         if [ "$is5ghz" ]; then
-            echo -e "\t${Blue}Opera en 5 GHz!${Color_Off}"
+            echo -e "\t${BBlue}Opera en 5 GHz!${Color_Off}"
         fi
 
         printonce="\tEstaciones:"
@@ -86,7 +110,7 @@ if [ "$(echo $?)" == "0" ]; then
                     printonce=''
                 fi
 
-                echo -e "\t\t${Green}" `echo $clientsMAC | cut -d ',' -f 1` "${Color_Off}"
+                echo -e "\t\t${BGreen}" `echo $clientsMAC | cut -d ',' -f 1` "${Color_Off}"
                 MAC2=`echo "$clientsMAC" | sed 's/ //g' | sed 's/-//g' | sed 's/://g' | cut -c1-6`
     
                 result2="$(grep -i -A 1 ^$MAC2 ./oui.txt)";
@@ -100,11 +124,11 @@ if [ "$(echo $?)" == "0" ]; then
                     fi
     
                     if [ "$warning" ]; then
-                        echo -e "\t\t\t${Purple}El dispositivo soporta el modo monitor${Color_Off}"
+                        echo -e "\t\t\t${BPurple}El dispositivo soporta el modo monitor${Color_Off}"
                     fi
     
                 else
-                    echo -e "\t\t\tVendor: ${Black}${On_White}Información no encontrada en la base de datos${Color_Off}"
+                    echo -e "\t\t\tVendor: ${BBlack}${On_White}Información no encontrada en la base de datos${Color_Off}"
                 fi
     
                 probed=`echo $line2 | cut -d ',' -f 7`
@@ -117,13 +141,13 @@ if [ "$(echo $?)" == "0" ]; then
         
     done < <(grep -E '([A-Za-z0-9._: @\(\)\\=\[\{\}\"%;-]+,){14}' $FILE)
     
-    echo -e "\n${LBlue}Estaciones no asociadas:${Color_Off}\n"
+    echo -e "\n${BBlue}Estaciones no asociadas:${Color_Off}\n"
     
     while read -r line2 ; do
     
         clientsMAC=`echo $line2  | cut -d ',' -f 1`
     
-        echo -e "${Green}" `echo $clientsMAC | cut -d ',' -f 1` "${Color_Off}"
+        echo -e "${BGreen}" `echo $clientsMAC | cut -d ',' -f 1` "${Color_Off}"
         MAC2=`echo "$clientsMAC" | sed 's/ //g' | sed 's/-//g' | sed 's/://g' | cut -c1-6`
     
         result2="$(grep -i -A 1 ^$MAC2 ./oui.txt)";
@@ -136,10 +160,10 @@ if [ "$(echo $?)" == "0" ]; then
                 echo -e "\t${Yellow}Es probable que se trate de un dispositivo móvil${Color_Off}"
             fi
             if [ "$warning" ]; then
-                echo -e "\t${Purple}El dispositivo soporta el modo monitor${Color_Off}"
+                echo -e "\t${BPurple}El dispositivo soporta el modo monitor${Color_Off}"
             fi
         else
-            echo -e "\tVendor: ${Black}${On_White}Información no encontrada en la base de datos${Color_Off}"
+            echo -e "\tVendor: ${BBlack}${On_White}Información no encontrada en la base de datos${Color_Off}"
         fi
 
         probed=`echo $line2 | cut -d ',' -f 7`
@@ -150,5 +174,13 @@ if [ "$(echo $?)" == "0" ]; then
     
     done < <(grep -E '(not associated)' $FILE)
 else
-    echo -e "\n${LBlue}[${Yellow}!${LBlue}] ${LGray}Archivo oui.txt no encontrado, descárgalo desde aquí: ${LBlue}http://standards-oui.ieee.org/oui/oui.txt${Color_Off}\n"
+    echo -e "\n${LBlue}[${BYellow}!${LBlue}] ${BGray}Archivo oui.txt no encontrado, descargando..."
+    wget http://standards-oui.ieee.org/oui/oui.txt > /dev/null 2>&1
+    if [ "$(echo $?)" == "0" ]; then
+        echo -e "\n${LBlue}[${BGreen}✔${LBlue}] ${LGreen}Se ha descargado el archivo correctamente. Vuelve a ejecutar el script.${Color_Off}"
+        exit 0
+    else
+        echo -e "\n${LBlue}[${BRed}✘${LBlue}] ${BRed}Error al descargar el archivo automaticamente, descargalo manualmente desde: ${LBlue}http://standards-oui.ieee.org/oui/oui.txt${Color_Off}\n"
+        exit 1
+    fi
 fi
